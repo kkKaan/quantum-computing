b9ec11d66484eef8fa9d323b28c2e50bbdf979d5
kkKaan <kaan.karacanta@gmail.com>
2024-12-12 16:28:37 +0300

    added density

--- source/simon/simon_amazon_test.py
+++ source/simon/simon_amazon_test.py
@@ -20,6 +20,7 @@ def get_all_combinations(vectors):
     The following function generates all possible combinations of the vectors
     """
     n = len(vectors[0])
+    print('The number of vectors is: ' + str(len(vectors)))
     return list(itertools.combinations(vectors, n))
 
 
@@ -45,6 +46,25 @@ def get_independent_set(samples):
     for combination in all_combinations:
         if is_independent_set(combination):
             return combination
+
+
+def get_all_independent_sets(samples):
+    """
+    The following function gets samples of n-sized strings from running the quantum part and return an n-1 x n matrix,
+    """
+    all_combinations = get_all_combinations(samples)
+    independent_sets = []
+    for combination in all_combinations:
+        if is_independent_set(combination):
+            independent_sets.append(combination)
+    return independent_sets
+
+
+def calculate_density(matrix):
+    """
+    The following function calculates the density of a matrix
+    """
+    return np.count_nonzero(matrix)
 
 
 def find_most_sparse_matrix(matrices):
@@ -73,15 +93,20 @@ def get_secret_integer(matrix):
     """
     gf_v = GF(matrix)  # Converting to a matrix over Z_2
 
+    # print('The matrix over GF(2) is:')
+    # for a in gf_v:
+    #     print(a)
+
     ### Keep the time of the computation below
     start_time = time.time()
     null_space = gf_v.T.left_null_space()  # Finding the left null space of the matrix
-    null_vector = np.array(null_space)[0]  # Extract the first null space vector
-    print("--- %s seconds ---" % (time.time() - start_time))  # Print the time taken for the computation
+    total_time = time.time() - start_time
+    print("--- %s seconds ---" % total_time)  # Print the time taken for the computation
     ###
 
+    null_vector = np.array(null_space)[0]  # Extract the first null space vector
     binary_string = "".join(null_vector.astype(int).astype(str))  # Ensure binary conversion
-    return binary_string
+    return binary_string, total_time
 
 
 if __name__ == '__main__':
@@ -123,10 +148,10 @@ if __name__ == '__main__':
         # Add the count to that of the of truncated bit string
         new_results[trunc_bitstring] = new_results.get(trunc_bitstring, 0) + count
 
-    plt.bar(new_results.keys(), new_results.values())
-    plt.xlabel('bit strings')
-    plt.ylabel('counts')
-    plt.xticks(rotation=70)
+    # plt.bar(new_results.keys(), new_results.values())
+    # plt.xlabel('bit strings')
+    # plt.ylabel('counts')
+    # plt.xticks(rotation=70)
     # plt.show()
 
     # These new results are independent of the secret string s, and n linearly independent of them
@@ -155,10 +180,57 @@ if __name__ == '__main__':
         print(a)
 
     # Get the secret integer
-    secret_int = get_secret_integer(ind_v)
+    secret_int, passed_time = get_secret_integer(ind_v)
     print('The secret integer is: ' + str(secret_int))
     print('The secret string is: ' + s)
     if (secret_int == s):
         print('We found the correct answer.')
     else:
         print('Error. The answer is wrong!')
+
+    # print("test gf")
+
+    # Test the Galois Field
+    # gf = galois.GF(11)
+
+    # # create 6x6 matrix with numbers smaller than 11
+    # test_matrix = np.random.randint(11, size=(6, 6))
+    # print('The matrix is:')
+    # for a in test_matrix:
+    #     print(a)
+
+    # gf_v = gf(test_matrix)
+    # print('The matrix over GF(11) is:')
+    # for a in gf_v:
+    #     print(a)
+
+    # start_time = time.time()
+    # null_space = gf_v.T.left_null_space()
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    # print('The left null space of the matrix is:')
+    # for a in null_space:
+    #     print(a)
+
+    print("------------------------------")
+
+    independent_sets = get_all_independent_sets(string_list)
+    print('The number of independent sets is: ' + str(len(independent_sets)))
+    times_and_density = []
+    for matrix in independent_sets:
+        # Get sparcity of the matrix and total time passed, give a graph showing these
+        density = calculate_density(matrix)
+        found_secret, found_time = get_secret_integer(matrix)
+        if found_secret == s:
+            times_and_density.append((density, found_time))
+
+    # Sort the list of tuples based on the density
+    times_and_density.sort(key=lambda x: x[0])
+
+    # Plot the graph
+    x = [x[0] for x in times_and_density]
+    y = [x[1] for x in times_and_density]
+    plt.plot(x, y)
+    plt.xlabel('Density')
+    plt.ylabel('Time')
+    plt.yscale('log')
+    plt.show()
