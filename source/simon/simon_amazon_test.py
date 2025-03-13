38155bcf90c0974eb3cacd6ef9e3a0ef956a60e6
kkKaan <kaan.karacanta@gmail.com>
2025-03-13 14:49:15 +0300

    two valid solutions for large secret strings

--- source/simon/simon_amazon_test.py
+++ source/simon/simon_amazon_test.py
@@ -419,7 +419,7 @@ def get_secret_integer_generic(matrix, mod=None):
 
 if __name__ == '__main__':
     # Define the secret string for Simon's algorithm (you can experiment with different strings)
-    s = '101101001110'
+    s = '10110100110010'
     n = len(s)
 
     circ = Circuit()
